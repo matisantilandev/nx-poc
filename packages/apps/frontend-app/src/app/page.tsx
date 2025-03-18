2641d47ce054031d28ec5a594ae85b33aaac2a8e
matisantillan11 <matias1.santillan@gmail.com>
2025-03-18 20:11:23 -0300

    Add frontend application and UI library with initial setup and components

---
+++ packages/apps/frontend-app/src/app/page.tsx
@@ -0,0 +1,6 @@
+'use client';
+import { UiLibrary } from '@gilly/ui-library';
+
+export default function Index() {
+  return <UiLibrary />;
+}
---
+++ packages/libs/ui-library/src/lib/ui-library.tsx
@@ -0,0 +1,9 @@
+export function UiLibrary() {
+  return (
+    <div className="w-full h-screen bg-black flex items-center justify-center">
+      <h1 className="text-white font-bold text-2xl">Welcome!</h1>
+    </div>
+  );
+}
+
+export default UiLibrary;
