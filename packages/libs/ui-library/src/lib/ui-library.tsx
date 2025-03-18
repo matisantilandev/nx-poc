a0de687e6a8f4e9d147417fdbbe59f49c447c033
matisantillan11 <matias1.santillan@gmail.com>
2025-03-18 20:23:56 -0300

    Update welcome message in UI library component

--- packages/libs/ui-library/src/lib/ui-library.tsx
+++ packages/libs/ui-library/src/lib/ui-library.tsx
@@ -1,7 +1,9 @@
 export function UiLibrary() {
   return (
     <div className="w-full h-screen bg-black flex items-center justify-center">
-      <h1 className="text-white font-bold text-2xl">Welcome!</h1>
+      <h1 className="text-white font-bold text-2xl">
+        Welcome. This is being render from the library!
+      </h1>
     </div>
   );
 }
